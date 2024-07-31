8309d470ddaad7fa17c070d02dedac2ab60caaad
juanvidev1 <juanvidev1@gmail.com>
2024-07-31 09:57:32 -0500

    style: Extract request options objetc from the fetch execution

--- privJs/client.js
+++ privJs/client.js
@@ -1,13 +1,15 @@
 class Client {
   async postData(url = "", data = {}) {
-    const response = await fetch(url, {
+    const reqOptions = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: "Basic VXNlckNhcmlIYXBweURQUzpEOVMqcFB5aDQx",
       },
       body: JSON.stringify(data),
-    });
+    };
+
+    const response = await fetch(url, reqOptions);
     return response.json();
   }
 }
